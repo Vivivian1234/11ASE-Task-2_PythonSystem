bc2d973300047861fda67d12578f35c92a2cd1bf
Vivivian1234 <160200931+Vivivian1234@users.noreply.github.com>
2025-05-15 10:09:11 +1000

    bam
    that's crazy

---
+++ story_TEST.py
@@ -0,0 +1,4 @@
+
+
+print ("You wake up, the last thing you remember is suffocating in gassy fabric, closing in from behind. This place looks familiar, like you’ve been here before, but you can't seem to remember anything. The yellowish wallpaper and the cold floor all feel so cold and lonely, but that doesn't matter now.")
+name = input("\nYour name... What is your name? Why can't you remember anything and yet everything seems so familiar?\n")
