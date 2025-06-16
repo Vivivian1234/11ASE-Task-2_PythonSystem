70db16a3cc46fef8a065202c932ed080e27ae9ad
Vivivian1234 <vivian.ding1@education.nsw.gov.au>
2025-06-16 11:10:19 +1000

    did some work yippee
    hallwaynorth, cafe, andyroom, working on partyroom
    
    i was a little productive for once??? that's crazy

--- test.py
+++ test.py
@@ -60,10 +60,23 @@ def inventory():
                   print("You browse the endless delicious recipes in the cokbook, your stomach rumbling in longing. You can't look for even a few seconds before you realise how hungry you are. You put the book away, trying your best to not be distracted.")
 
                 elif use == "coffee":
-                  print("You hold the warm cup of coffee in your hands, the aroma heavenly. You would drink it... but you're severely allergic to coffee. A pity.")
+                  print("You hold the warm cup of coffee in your hands, the aroma heavenly. You would drink it... but rememeber that you're severely allergic to coffee. A pity.")
                 
                 elif use == "lever":
                   print("You pull out the small and suprisingly light metal lever from your pocket. It reminds you a but of a Minecraft lever, and you wonder where that thought came fron. Anyway, you ponder where this could be used...")
+
+                elif use == "coin":
+                  print("The small coin is shiny and warm in your hands. It's fun to play with, as much fun as a coin can be in such a dull environment. Losing it wouldn't be the best idea...you put it away for now.")
+
+                elif use == "note":
+                  print("You take out the scribbled note. It reads:")
+                  print("'Look at me and I'll look at you,") 
+                  print("No matter what, no matter who,")
+                  print("You'll always see that there'll be two.")
+                  print("To find the key, you must know yourself well.")
+                  print("If you don't, you'll be locked in this cell!")
+                  print("What am I?'")
+                  print("You think about this riddle, and a few answers are possible...but only one answer appears clear as day. You know it...and it's in the bathrooom!")
 
             else:
                 print(f"\nYou don't have any {use}s left!")
@@ -72,9 +85,13 @@ def inventory():
 
 
 def main():
-  print("What is your name?\n")
-  print("Good, at least you remember your name.\n")
+  print("You wake up, the last thing you remember is suffocating in gassy fabric, closing in from behind. This place looks familiar, you have definitely been here sometime before, but you can't seem to remember anything. The yellowish wallpaper and the cold floor all feel so lonely, but that doesn't matter now. Why can't you remember anything and yet everything seems so familiar?")
   
+  name = input("What is your name?\n").lower()
+  name = name.capitalize()
+
+  print("Good, at least you remember your name.\n")
+
   Cellar()
 
 def Cellar():
@@ -139,6 +156,7 @@ def DogRoom():
         action = input("\nWhat do you want to do? Options: use, s\n").lower()
 
         if action == "use":
+
             if "bone" not in player_inventory:
                 print("There is nothing helpful in your inventory right now, best come back later to find something suitable.")
             else:
@@ -185,8 +203,10 @@ def Bathroom():
         print("You enter a broken down bathroom, completely unusable. There’s mold on the toilet and sink, the shower head is smashed. The once-white tiles are now brown, covered in dirt and grime. And… what is that white thing sticking out of the sink?")
         print("You approach the sink, and see a bone lying inside. It doesn't seem human...maybe it came from a butcher? Hopefully? You're unsure when you place it in your backpack.")
         print("'bone' added to our backpack.")
-        player_inventory["bone"] = 1
+        player_inventory["bone"] = player_inventory.get("bone", 0) + 1
         print("You feel like there is something hiding in here, but you don't know what. Maybe come back later when you have a few more items in your inventory that may be helpful.")
+        print("\nYour updated backpack:")
+        inventory()
         print("You exit the room.")
         HallwayEast()
         return
@@ -214,7 +234,7 @@ def Bathroom():
                     HallwayEast()
                     return
                 else:
-                    print(f"You try to use the {use_what}, but it does nothing.")
+                    print(f"You try to use the {use_what}, but it does nothing. You stare at the cracked relfection of you in the mirror, and you swear it is looking at you weirdly.")
             elif action == "s":
                 print("You exit the room.")
                 HallwayEast()
@@ -227,5 +247,191 @@ def Bathroom():
         HallwayEast()
         return
 
+def HallwayNorth():
+    global current_room
+    current_room = "HallwayNorth"
+
+    while True:
+        print("\n---HallwayNorth---")
+        print("There are three rooms down this hallway, a party room to the east, a bedroom with a messy sign with 'Andy' scribbled on it to the west, and a cafe continuing north. You can also return to the cellar back to the south.")
+        action = input("\nWhat do you want to do? Options: show, n, e, s, w\n").lower()
+        if action == "show":
+          inventory()
+        elif action == "n":
+          Cafe()
+        elif action == "e":
+          PartyRoom()
+        elif action == "s":
+          Cellar()
+        elif action == "w":
+          AndyRoom()
+        else:
+          print("You don't think you can go that way right now.")
+        
+def Cafe():
+    global current_room
+    current_room = "Cafe"
+
+    while True:
+        print("---Cafe---")
+
+        if "coffee" in player_inventory:
+            print("You look around. You do not need to do anything here. Friendly Barista is staring blankly at a wall. Let's give the coffee to Andy to get the clue about the keys. You decide to leave.")
+            HallwayNorth()
+            return
+
+        if "note" in player_inventory:
+            print("You look around. You do not need to do anything here. Friendly Barista is staring blankly at a wall. Has Andy been down here this whole time? If so, surely he has talked the the Friendly Barista before to order more coffee right?")
+            print("You ask the barista about Andy. He smiles back blankly, not saying a single word, his eyes seemingly looking past you to the opposite wall. You decide to leave.")
+            HallwayNorth()
+            return
+
+        if "coin" in player_inventory:
+            print("You enter the brightly lit and calming cafe.")
+            print("A barista with calm, blank eyes greets you, staring into your eyes with something less than human.")
+            print("'Greetings, I am Friendly Barista. Would you like buy some coffee? It's one Cuppa Coin.'")
+            print("You see the huge coin drawing behind him on the wall, silver with a coffee cup engraved on it. The coin Andy gave you is identical.")
+            
+            action = input("\nWhat do you want to do? Options: use, s\n").lower()
+
+            if action == "use":
+                use_what = input("Use what?\n").lower()
+                if use_what == "coin":
+                    print("You hand over the small coin, and Friendly Baraista gives you piping hot coffee in a simple paper cup. You gladly accept the coffee and hold on to it. You can't drink it, because you just so happened to be allergic to coffee. Oh well, at least it's warm. You put it away. \n")
+                    
+                    player_inventory["coin"] -= 1
+                    if player_inventory["coin"] == 0:
+                        del player_inventory["coin"]
+
+                    player_inventory["coffee"] = player_inventory.get("coffee", 0) + 1
+
+                    print("You used the coin. 'coffee' added to your backpack.")
+                    print("\nYour updated backpack:")
+                    inventory()
+                    print("\nThere is nothing left to do here. You exit the room.")
+                    HallwayNorth()
+                    return
+                else:
+                    print(f"You try to use the {use_what}, but it does nothing. Friendly Barista stares at you, smiling.")
+            elif action == "s":
+                print("You exit the room.")
+                HallwayNorth()
+                return
+            else:
+               print("Invalid option.")
+
+        print("You enter a spotless and well furnished cafe, the clean wooden floors and light blue wallpaper decorated with a flower mural making it seem out of place, so comfortable and homely compared to the rest of this dark and gloomy underground structure you are in.")
+        print("A barista with calm, blank eyes greets you, staring into your eyes with something less than human.")
+        print("'Greetings, I am Friendly Barista. Would you like buy some coffee? It's one Cuppa Coin.'")
+        print("You see a huge coin drawing behind him on the wall, silver with a coffee cup engraved on it. That must be the currency to buy a coffee.")
+        print("You ask about the kidnapping and keys but he acts clueless, but still smiling as laid back as ever.")
+
+        action = input("\nWhat do you want to do? Options: use, s\n").lower()
+        
+        if action == "use":
+            print("There is nothing helpful in your inventory right now, best come back later to find something suitable.")
+        elif action == "s":
+            HallwayNorth()
+            return
+        else:
+            print("Invalid option.")
+
+def AndyRoom():
+    global current_room
+    current_room = "AndyRoom"
+
+    while True:
+        print("---AndyRoom---")
+
+        if "note" in player_inventory:
+            print("You enter the dirty room again. Andy is happily drinking the coffee, much less tired looking already. There isn't anything left here do, and you should leave Andy with this coffee in peace...")
+            print("You exit the room.")
+            HallwayNorth()
+            return
+        
+        if "coffee" in player_inventory:
+            print("You enter the room with a warm cup of coffee in your hands. Andy wakes up to the sound of footsteps, and you hand it to him.")
+            print("'Thanks!' he cheers, happily gulping down the coffee with great delight. 'Oh wait, right, the clue...'")
+            print("He pulls out a small note from his pants pocket and rummages through on of the drawers and pulls out what seems to be... a lever?")
+            print("He hands them both to you, when you give the small lever a confused look, he shrugs, returning to his coffee.")
+            print("You put the lever in your backback at read the scribbled note.")
+            print(" It reads:\n")
+            print("'Look at me and I'll look at you,") 
+            print("No matter what, no matter who,")
+            print("You'll always see that there'll be two.")
+            print("To find the key, you must know yourself well.")
+            print("If you don't, you'll be locked in this cell!")
+            print("What am I?'")
+            print("You think about this riddle, and a few answers are possible...but only one answer appears clear as day. You know it...and it's in the bathrooom!")
+
+            player_inventory["coffee"] -= 1
+            if player_inventory["coffee"] == 0:
+                del player_inventory["coffee"]
+
+            player_inventory["lever"] = player_inventory.get("lever", 0) + 1
+            player_inventory["note"] = player_inventory.get("note", 0) + 1
+
+            print("You used the coffee. 'lever' and 'note' added to your backpack.")
+            print("\nYour updated backpack:")
+            inventory()
+            print("\nThere is nothing left to do here. You exit the room.")
+            HallwayNorth()
+            return
+        
+        if "coin" in player_inventory:
+            print("You enter the dirty room again. Andy is still lying on his bed, and the softest of snores tell you he's somehow fallen asleep already. Best to go get him some coffee...")
+            print("You exit the room.")
+            HallwayNorth()
+            return
+        
+        print("You enter a dirty room full of mould, sweaty and dirty clothes, empty, unwashed coffee mugs and yellowed open books. It reeks of coffee and expired milk. You see a tired guy in a bad mood sitting on his bed rubbing his face.")
+        print("'Ugh... I'm so tired...' he groans.")
+        print("You ask about the keys.")
+        print("'Oh... gimme a coffee and I'll give you a clue to the key...zzz' he lies flat on his bed, yawning widely.")
+        print("'Oh right...take these. The name's Andy by the way.' He hands you a small silver coin, with a small coffee cup engraved on it.")
+        print("Is this...money? Is this how you buy the coffee? Alright...you put it away.")
+
+        player_inventory["coin"] = player_inventory.get("coin", 0) + 1
+
+        print("\nYour updated backpack:")
+        inventory()
+        print("\nBest to go get Andy some coffee.. You exit the room.")
+        HallwayNorth()
+        return
+
+def PartyRoom():
+    global current_room
+    current_room = "PartyRoom"
+
+    while True:
+        print("---PartyRoom---")
+        
+        print("You enter a brightly coloured room with helium balloons and a tall clown standing by opposite wall, who is wearing a bright rainbow wig and a giant red nose. The tall clown is smiling a big, bright, red smile.")
+        print("You walk across the vividly decorated room, and can feel the clown's eyes following you, making eye contact when you glance at it nervously. He does not provoke you, watching quietly as you approach the table in the centre of the room.")
+        print("On the table, there is a 5-layer birthday cake.")
+        print("It looks and smells a bit stale, but would still undoubtably be delicious.")
+        print("Your mouth waters as you approach it, but as you get closer, you realise the sugary icing spells out 'HAPPY BIRTHDAY; DO NOT EAT ME'.")
+        print("The cake looks so appetizing, and you're so hungry.")
+        print("Would it hurt just to take a single slice?")
+        print("Should you go talk to the quiet clown watching you?")
+
+        action = input("\nWhat do you want to do? Options: show, eat, talk, w\n").lower()
+
+        if action == "show":
+            inventory()
+        elif action == "eat":
+            print("----------")
+        elif action == "talk":
+            print("You try to start a conversation with the tall clown that is eyeing you. You ask about the 5-layer cake with the strange iced message. The tall clown nods, and warns you not to eat the cake. He looks like he is on the verge of tears, his watery eyes sparkling and his delicate voice trembling. Was eating the cake that dangerous? Oh well. The clown seems really desperate for you to not eat the cake, so you leave the cake and the party room. As you close the door, you hear a huge sigh of relief from the tall clown.")
+            print("You exit the room.")
+            HallwayNorth()
+            return
+        elif action == "w":
+            HallwayNorth()
+            print("You don't think you can go that way right now.")
+
 
 main()
+  
+## UP TO PARTYROOM - EAT
+## MAKE IT SO THEY HAVE TO EAT A NUMBER OF SLICES AND THEN THEY FAINT AND RESET THE WHOLE THING
